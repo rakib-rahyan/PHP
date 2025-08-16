ac87ac18991570d2b0b0176d93a0f5c92cee988b
rakib-rahyan <rakibrahyan20232@gmail.com>
2025-08-16 18:06:47 +0600

    what is the most sequre get or porst, post is the more suqure

--- index.php
+++ index.php
@@ -7,16 +7,41 @@
 </head>
 <body>
     <form action="index.php" method="post">
+        <label for="">quantity: <label>
+        <input type="text"name = "quantity">
+        <input type="submit" value="total"><br>
+    </form>
+
+
+
+
+
+    <!--
+1st
+    <form action="index.php" method="post">
         <label> username: </label>
         <input type = "text" name = "username"> <br>
         <label >password</label>
         <input type="password" name="password"><br>
         <input type="submit" value="Log In">
     </form>
+    -->
+
 </body>
 </html>
 
+
 <?php
+/*
+
+1st
     echo "{$_POST["username"]}<br>";
     echo "{$_POST["password"]}<br>";
+*/
+    $item = "przza";
+    $price = 5.99;
+    $quantity = $_POST["quantity"];
+    $total = $price * $quantity;
+    echo "You have ordered {$quantity} x {$item}/s <br>";
+    echo "Your total is \${$total}";
 ?>
